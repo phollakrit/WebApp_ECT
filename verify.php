8d0b6913ed5de16b08c1ead599671567a8d417ea
pat123 <phollakrit@hotmail.com>
2022-09-01 14:44:09 +0700

    work3

---
+++ verify.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 align="center">Webboard KakKak</h1>
+    <hr>
+    <div align="center">
+        เข้าสู่ระบบด้วย<br>
+        Login = <?php echo $_POST["login"];?><br>
+        Password = <?php echo $_POST["pwd"];?><br>
+
+        <?php 
+        //echo "Login = ".$_POST["login"]."<br>";
+        //echo "Password = ".$_POST["pwd"];
+        ?>
+    </div>
+</body>
+</html>
